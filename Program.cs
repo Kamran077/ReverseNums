0314d7d6c0b4bc770ef7e693b977897aa7789f88
amran <kamrane15012023@gmail.com>
2024-05-13 21:30:55 +0400

    File.txt

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseNums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ededi daxil et : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            string eded = Convert.ToString(num);
+
+            for (int i = eded.Length - 1; i >= 0; i--)
+            {
+                Console.Write(eded[i] + " ");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
